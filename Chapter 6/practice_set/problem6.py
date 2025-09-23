2fda20c7b2bc8bdb0139568da30e8fa6b38749a5
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-09-23 22:20:21 +0530

    Adding my Python Notes

---
+++ Chapter 6/practice_set/problem6.py
@@ -0,0 +1,18 @@
+marks = int(input("Enter marks:"))
+
+if(91 > marks > 100):
+    print("Excellent")
+elif(81 > marks > 90):
+    print("A")
+
+if(71 > marks > 80):
+    print("B")
+
+if(61 > marks > 70):
+    print("C")
+
+if(51 > marks > 60):
+    print("D")
+
+else:
+    print("F")
