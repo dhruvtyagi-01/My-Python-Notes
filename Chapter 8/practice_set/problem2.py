910d3551229f01c779bf464f70e2084dd1049e25
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-09-25 21:32:38 +0530

    Adding my Python Notes

--- Chapter 8/practice_set/problem2.py
+++ Chapter 8/practice_set/problem2.py
@@ -7,7 +7,7 @@
 def convert(C):
     F = (C * 1.8)+32
     F = round(F, 2)
-    print(f"Temperature in fahrenheit will be {F}")
+    print(f"Temperature in fahrenheit will be {F}°F")
 
 C = float(input("Enter temp in celsius to change in fahrenheit:"))
 
