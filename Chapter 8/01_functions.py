224a597a91c1d1e2cea1e70e4871e62b12451e42
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-09-25 18:56:18 +0530

    Adding my Python Notes

---
+++ Chapter 8/01_functions.py
@@ -0,0 +1,9 @@
+def avg():
+    a = int(input("Enter number:"))
+    b = int(input("Enter number:"))
+    c = int(input("Enter number:"))
+
+    average = (a + b + c) / 3
+    print("The average is",average)
+
+avg()
