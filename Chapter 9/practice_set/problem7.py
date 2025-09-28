594a295531e8312b29e0d34f59e6af2779d39e1d
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-09-28 12:07:10 +0530

    Adding my Python Notes

---
+++ Chapter 9/practice_set/problem7.py
@@ -0,0 +1,10 @@
+with open("file(a).txt") as f:
+    content_A = f.read()
+
+with open("file(b).txt") as f:
+    content_B = f.read()
+
+if(content_A == content_B):
+    print("Both files are identical and match content")
+else:
+    print("Files are not identical and do not match content")
