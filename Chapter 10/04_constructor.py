c65f86525c855caeb3bcb09e0aece405277bfd34
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-09-30 23:34:21 +0530

    Adding my Python Notes

---
+++ Chapter 10/04_constructor.py
@@ -0,0 +1,14 @@
+class Employee: 
+    language = "Py"
+    salary = 1200000
+
+    def __init__(self, name, salary, language):  # Dunder method which is automatically called
+        self.name = name 
+        self.salary = salary
+        self.language = language
+        print("I created an object")
+
+dhruv = Employee("Dhruv", 1200000, "C")
+
+# dhruv.name = "Dhruv"
+print(dhruv.name, dhruv.salary, dhruv.language)
