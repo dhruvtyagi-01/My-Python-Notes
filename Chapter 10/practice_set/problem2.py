0365998134de73d177202347c3c7bce48e4267e7
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-02 15:41:25 +0530

    Adding my Python Notes

---
+++ Chapter 10/practice_set/problem2.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def __init__(self, n):
+        self.n = n
+
+    def square(self):
+        print(f"Square is {pow(self.n, 2)}")
+
+    def cube(self):
+        print(f"Cube is {pow(self.n, 3)}")
+
+    def square_root(self):
+        print(f"Square root is {pow(self.n, 1/2)}")
+
+
+value = float(input("Enter value to calculate:"))
+a = Calculator(value)
+
+a.square()
+a.cube()
+a.square_root()
