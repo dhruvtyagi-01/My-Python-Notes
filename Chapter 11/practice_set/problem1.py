60bf8b5887370e68c3b038c9f584958fa4379d82
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-02 23:16:53 +0530

    Adding my Python Notes

---
+++ Chapter 11/practice_set/problem1.py
@@ -0,0 +1,23 @@
+class TWODvector:
+    
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+    
+    def show(self):
+        print(f"The vector is {self.i}i + {self.j}j")
+
+class THREEDvector(TWODvector):
+    def __init__(self, i, j, k):
+        super().__init__(i, j)
+        self.k = k
+
+        
+    def show(self):
+        print(f"The vector is {self.i}i + {self.j}j + {self.k}k")
+
+a = TWODvector(3, 6)
+a.show()
+
+b = THREEDvector(5, 8, 9)
+b.show()
