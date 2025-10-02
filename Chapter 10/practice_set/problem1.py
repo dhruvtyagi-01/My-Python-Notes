91929cc104ae7687c537c71d2d6c1e1d83e519fe
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-02 13:59:01 +0530

    Adding my Python Notes

---
+++ Chapter 10/practice_set/problem1.py
@@ -0,0 +1,12 @@
+class Programmer:
+    company = "Microsoft"
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+
+p = Programmer("Dhruv", 1200000, 245001)
+print(p.name, p.salary, p.company)
+
+r = Programmer("Rohan", 1200000,)
+print(r.name, r.salary, r.company)
