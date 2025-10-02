a2730d29bc1d3ed4fb4bfd6232a8a4ac144614c5
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-02 16:10:28 +0530

    Adding my Python Notes

---
+++ Chapter 10/practice_set/problem4.py
@@ -0,0 +1,10 @@
+class Employee: 
+    language = "Py"
+    salary = 1200000
+
+    def getInfo(dhruv):
+        print(f"Language is {dhruv.language} and salary is {dhruv.salary}")
+
+dhruv = Employee()
+
+dhruv.getInfo()
