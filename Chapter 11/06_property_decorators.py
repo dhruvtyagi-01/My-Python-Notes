c1b8f6399d5dc143b8713e78333826494874d7ca
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-02 22:43:25 +0530

    Adding my Python Notes

---
+++ Chapter 11/06_property_decorators.py
@@ -0,0 +1,14 @@
+class Demo:
+
+    @property 
+    def name(self): 
+        return f"{self.fname} {self.lname}"
+    
+    @name.setter 
+    def name (self,value): 
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+
+e = Demo()
+e.name = "Dhruv Tyagi"
+print(e.name)
