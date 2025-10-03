835da823851c893497bc93aaeb3a9b544658a00f
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-03 21:51:38 +0530

    Adding my Python Notes

--- Chapter 12/practice_set/problem3.py
+++ Chapter 12/practice_set/problem3.py
@@ -4,5 +4,7 @@ table = [n * i for i in range(1, 11)]
 
 print(table)
 
-with open("table.txt", "w") as f:
-    f.write(table)
+table = str(table)
+
+with open("table.txt", "a") as f:   # "a" so it won’t overwrite
+    f.write(f"\nTable of {n}: {table}")
