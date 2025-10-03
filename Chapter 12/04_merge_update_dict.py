389fd6ac2a5da0a783c2894a81bfb936d94a06bc
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-03 16:09:04 +0530

    Adding my Python Notes

---
+++ Chapter 12/04_merge_update_dict.py
@@ -0,0 +1,8 @@
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+
+merged = dict1 | dict2
+dict2 |= dict1
+
+print(merged) # Output: {'a': 1, 'b': 3, 'c': 4}
+print(dict2)  # Output: {'b': 2, 'c': 4, 'a': 1}
