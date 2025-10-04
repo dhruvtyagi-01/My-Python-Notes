0780711e9bd33de7acc955fbb22a50a0f72073d1
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-04 16:35:13 +0530

    Adding my Python Notes

---
+++ Chapter 13/practice_set/problem1.py
@@ -0,0 +1,6 @@
+name = input("Enter name:")
+marks = int(input("Enter name:"))
+phoneNo = int(input("Enter name:"))
+
+info = "The name of the student is {}, his marks are {} and phone number is {}".format(name, marks, phoneNo)
+print(info)
