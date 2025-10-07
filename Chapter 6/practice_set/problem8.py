b3755a796cc7395e4f2f32d527432507918a26c4
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-07 15:16:19 +0530

    Adding my Python Notes

---
+++ Chapter 6/practice_set/problem8.py
@@ -0,0 +1,25 @@
+state = ""
+
+while(True):
+    inp = input(">")
+    if(inp.lower() == 'help'):
+        print("start - to start the car")
+        print("stop - to stop the car")
+        print("quit - to exit")
+    elif inp.lower() == 'start':
+        if(state == "start"):
+            print("car is already moving!")
+        else:
+            print("The car has started , READY TO GO :)")
+            state = inp
+    elif inp.lower() == 'stop':
+        if state == "stop":
+            print("Car is already stopped!")
+        else:
+            print("You stopped the car :(")
+            state = inp
+    elif inp.lower() == 'quit':
+        print("Game ended!!")
+        break
+    else:
+        print("Invalid input")
