cab49fd4fb1d5db7d0813a09b498491ac1e107bc
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-09 11:56:07 +0530

    Adding my Python Notes

--- Chapter 5/05_set_union_intersection.py
+++ Chapter 5/05_set_union_intersection.py
@@ -6,3 +6,17 @@ print(s1 | s2)
 
 # print(s1.intersection(s2))
 print(s1 & s2)
+
+# print(s1.difference(s2))
+print(s1 - s2)  # subtract set s2 from set s1
+
+# print(s2.difference(s1))
+print(s2 - s1)  # subtract set s1 from set s2
+
+# print(s1.symmetric_difference(s2))
+print(s1 ^ s2)
+
+print(sorted(s1 | s2))
+
+a = {20, 14, 12, 30}
+print(set(sorted(a, reverse=True)))
