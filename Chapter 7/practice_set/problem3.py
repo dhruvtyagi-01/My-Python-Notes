cc6af07cc2a6b5bdfafe7e843efff202eb66d897
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-10-09 12:00:46 +0530

    Adding my Python Notes

--- Chapter 7/practice_set/problem3.py
+++ Chapter 7/practice_set/problem3.py
@@ -1,7 +1,10 @@
 n = int(input("Enter number:"))
 
-if (n <= 1):
+if n == 0 or n == 1:
     print(f"{n} is not prime")
+
+elif n < 0:
+    print("Enter a positive number!")
 
 else:
     for i in range(2, n):
