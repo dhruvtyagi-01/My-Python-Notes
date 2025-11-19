8d10ab6059f2655dcc12b4778d35b3e0b6e240ae
dhruvtyagi-01 <dhruvvtyagii2007@gmail.com>
2025-11-19 12:40:46 +0530

    Adding my Python Notes

--- string_palindrome.py
+++ string_palindrome.py
@@ -1,10 +1,17 @@
 string = input("Enter string to check palindrome: ")
-rev = ""
+# rev = ""
 
-for item in string:
-    rev = item + rev
+# for item in string:
+#     rev = item + rev
     
-if(rev == string):
+# if(rev == string):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+rev = string[-1::-1]
+
+if rev == string:
     print("palindrome")
 else:
     print("not palindrome")
